a0804e4349b1c83e886eb491434d9fdeb0db0bf6
luparral <lparral@dc.uba.ar>
2018-11-12 18:46:02 -0300

    repetidos en filas y columnas

--- src/main/java/Sudoku.java
+++ src/main/java/Sudoku.java
@@ -1,6 +1,7 @@
 
 import java.util.*;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.tuple.Pair;
 
 public class Sudoku {
@@ -15,7 +16,7 @@ public class Sudoku {
     // Crear un soduku Aleatorio de tamaño dimension x dimension y una cantidad fixedQuantity de celdas con elementos fijos.
 
     public Sudoku(int dimension, int fixedQuantity) {
-        this.dim = dimension;
+        this.dim = dimension*dimension;
         this.board = new int[dim][dim];
         this.boardFixedPositions = new int[dim][dim];
         this.fixedQuantity = fixedQuantity;
@@ -25,30 +26,57 @@ public class Sudoku {
 
     }
 
-    public void show(){
-        for (int i = 0; i < dim; i++) {
-            for (int j = 0; j < dim; j++) {
-                System.out.println("position (" + i + ", " + j + ") = " + board[i][j]);
-                System.out.println("position fixed board (" + i + ", " + j + ") = " + boardFixedPositions[i][j]);
+    private void show() {
+        for (int row = 0; row < dim; row++) {
+            for (int column = 0; column < dim; column++) {
+                String fixedPos = "0";
+                if(boardFixedPositions[row][column] == 1) {
+                    fixedPos = "X"; // Si tiene una X es porque esa posición es fija.
+                }
+                System.out.print(board[row][column]+"("+fixedPos+")"+" ");
             }
+            System.out.println();
         }
     }
 
-//    public boolean hasRepetedElementsInRow(){
-//
-//    }
-//
-//    [i0j0][i0j1][i0j2][i0j3]
-//    [i1j0][i1j1][i1j2][i1j3]
-//    [i2j0][i2j1][i2j2][i2j3]
-//    [i3j0][i3j1][i3j2][i3j3]
-//
-//    public boolean hasRepetedElementsInColumn(){
-//
-//    }
+    public boolean hasDuplicatesElementsInRow(){
+        for (int i = 0; i < dim; i++) {
+            int[] row = this.board[i];
+            System.out.println("row " + i + " : " +  Arrays.toString(row));
+            if(hasDuplicates(row)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasDuplicatesElementsInColumn(){
+        for(int i=0; i<dim; i++){
+            int[] column = new int[dim];
+            for (int j=0; j<dim;j++) {
+                column[j] = (board[j][i]);
+            }
+            System.out.println("column " + i + " : " +  Arrays.toString(column));
+            if(hasDuplicates(column)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Object[] getColumn(Object[][] array, int index){
+        Object[] column = new Object[array[0].length]; // Here I assume a rectangular 2D array!
+        for(int i=0; i<column.length; i++){
+            column[i] = array[i][index];
+        }
+        return column;
+    }
+
 //    public boolean hasRepetedElementsInQuadrant(){
 //
 //    }
+
+
 
 
     private void initBoards(){
@@ -83,12 +111,26 @@ public class Sudoku {
 
 
     public static void main(String[] args){
-        int dim = 5;
-        int fixedQuantity = 5;
+        int dim = 2;
+        int fixedQuantity = 1;
         Sudoku sudoku = new Sudoku(dim, fixedQuantity);
         sudoku.show();
+        boolean noDupsRows = sudoku.hasDuplicatesElementsInRow();
+        System.out.println("duplicates in rows: " + noDupsRows);
+        boolean noDupsColumns = sudoku.hasDuplicatesElementsInColumn();
+        System.out.println("duplicates in columns: " + noDupsColumns);
+    }
 
-
+    private boolean hasDuplicates(int[] array) {
+        boolean duplicates=false;
+        for (int j = 0 ;j<array.length;j++) {
+            for (int k=j+1;k<array.length;k++) {
+                if (k!=j && array[k] == array[j]) {
+                    duplicates=true;
+                }
+            }
+        }
+        return duplicates;
     }
 
 }
